f7190a9fe77effff73d0eda437bb3793115e3b23
vitor-caixeta <vitorgfcaixeta@gmail.com>
2024-01-31 13:09:43 -0300

    Using components and the controller for rendering

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\RouteController@MainView');
 
 Route::get('/first-route', function () {
     return view('first-route');
